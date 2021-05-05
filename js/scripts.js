96703e44f3de13ebaaaede1fd376141f6e069470
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-05-05 11:29:45 +0200

    added Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
-
-alert('Hallo Welt!!');
-
-let favVeg = 'Radicchio';
-
-document.write(favVeg);
+let pokemonList = [
+                    {name: 'Kakuna', height: 0.6, type: ['bug', 'poison']},
+                    {name: 'Zubat', height: 0.8, type: ['poison', 'flying']},
+                    {name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
+                    {name: 'Yanma', height: 1.2, type: ['bug', 'flying']} ];
+          
