fa8bab95f994d06f44c3de68448d1054e7ee809b
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-05-06 16:39:12 +0200

    up to 1.3
    Pokemon app up to 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,19 @@ let pokemonList = [
                     {name: 'Kakuna', height: 0.6, type: ['bug', 'poison']},
                     {name: 'Zubat', height: 0.8, type: ['poison', 'flying']},
                     {name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
-                    {name: 'Yanma', height: 1.2, type: ['bug', 'flying']} ];
-          
+                    {name: 'Yanma', height: 1.2, type: ['bug', 'flying']}
+                     ];
+
+
+                  
+let text = "WOW! That's big!";
+for (let i=0; i< pokemonList.length; i++) {
+   if (pokemonList[i].height > 1.2) {
+    document.write(`${pokemonList[i].name}: (Height: ${pokemonList[i].height} m, Types: "${pokemonList[i].type[0]}" and "${pokemonList[i].type[1]}") - ${text} <br>`)
+    } else {
+    document.write(`${pokemonList[i].name}: (Height: ${pokemonList[i].height} m, Types: "${pokemonList[i].type[0]}" and "${pokemonList[i].type[1]}") <br>`)
+    }
+}
+
+
+
