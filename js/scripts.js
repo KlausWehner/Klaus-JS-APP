88720df2acc33985365743c0797735727127d583
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-05-12 14:37:26 +0200

    IIFE added
    Exercise Part 2:
    - IIFE
    - getAll
    - add
    functions added

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,43 @@
-
-let pokemonList = [
-                    {name: 'Kakuna', height: 0.6, type: ['bug', 'poison']},
-                    {name: 'Zubat', height: 0.8, type: ['poison', 'flying']},
-                    {name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
-                    {name: 'Yanma', height: 1.2, type: ['bug', 'flying']}
-                     ];
-
-
-                  
-let text = "WOW! That's big!";
-
-/*
-for (let i=0; i< pokemonList.length; i++) {
-   if (pokemonList[i].height > 1.2) {
-    document.write(`<p class="poke-list"> ${pokemonList[i].name}: (Height: ${pokemonList[i].height} m, Types: "${pokemonList[i].type[0]}" and "${pokemonList[i].type[1]} ) - ${text} </p>`)
-    } else {
-    document.write(`<p class="poke-list"> ${pokemonList[i].name}: (Height: ${pokemonList[i].height} m, Types: "${pokemonList[i].type[0]}" and "${pokemonList[i].type[1]})  </p>`)
+let pokemonRepository = (function () {
+    let pokemonList = [
+                      {name: 'Kakuna', height: 0.6, type: ['bug', 'poison']},
+                      {name: 'Zubat', height: 0.8, type: ['poison', 'flying']},
+                      {name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
+                      {name: 'Yanma', height: 1.2, type: ['bug', 'flying']}
+                       ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
     }
-*/
-
-Object.keys(pokemonList).forEach(function(property) {
-  if (pokemonList[property].height > 1.2) {
-    document.write('<p>' + 'Name: ' + pokemonList[property].name + ', Height: ' + pokemonList[property].height + 'm,' + ' (' + text + ')' + ' Type: ' + '<i>' + pokemonList[property].type[0] + ' ' + pokemonList[property].type[1] + '</i>' + '.' +'</p>');
-    } else {
-    document.write('<p>' + 'Name: ' + pokemonList[property].name + ', Height: ' + pokemonList[property].height + 'm,' + ' Type: ' + '<i>' + pokemonList[property].type[0] + ' ' + pokemonList[property].type[1] + '</i>' + '.' +'</p>');  
+  
+    function getAll() {
+      return pokemonList;
     }
-})  
-
-
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();  
+  
+ 
+                    
+  let text = "WOW! That's big!";
+  
+  
+  
+  let pokemonList = pokemonRepository.getAll();
+  
+  
+  
+  Object.keys(pokemonList).forEach(function(property) {
+    if (pokemonList[property].height > 1.2) {
+    document.write('<p>' + 'Name: ' + pokemonList[property].name + ', Height: ' + pokemonList[property].height + 'm,' + ' (' + text + ')' + ' Type: ' + '<i>' + pokemonList[property].type[0] + ',  ' + pokemonList[property].type[1] + '</i>' + '.' +'</p>');
+    } else {
+    document.write('<p>' + 'Name: ' + pokemonList[property].name + ', Height: ' + pokemonList[property].height + 'm,' + ' Type: ' + '<i>' + pokemonList[property].type[0] + ',  ' + pokemonList[property].type[1] + '</i>' + '.' +'</p>');  
+    }
+  })
+  
    
 
 
