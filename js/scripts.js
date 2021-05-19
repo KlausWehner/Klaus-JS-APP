4dff754adad7b9479fba30b45e354bf70456e0ca
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-05-19 16:50:41 +0200

    submission 1.6
    add function,
    event listener and styling added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,60 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-                      {name: 'Kakuna', height: 0.6, type: ['bug', 'poison']},
-                      {name: 'Zubat', height: 0.8, type: ['poison', 'flying']},
-                      {name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
-                      {name: 'Yanma', height: 1.2, type: ['bug', 'flying']}
-                       ];
-  
-    function add(pokemon) {
+  let pokemonList = [
+                    {name: 'Kakuna', height: 0.6, type: ['bug', 'poison']},
+                    {name: 'Zubat', height: 0.8, type: ['poison', 'flying']},
+                    {name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
+                    {name: 'Yanma', height: 1.2, type: ['bug', 'flying']}
+                     ];
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'type' in pokemon
+    ) {
       pokemonList.push(pokemon);
-    }
-  
-    function getAll() {
-      return pokemonList;
-    }
-  
-    return {
-      add: add,
-      getAll: getAll
-    };
-  })();  
-  
- 
-                    
-  let text = "WOW! That's big!";
-  
-  
-  
-  let pokemonList = pokemonRepository.getAll();
-  
-  
-  
-  Object.keys(pokemonList).forEach(function(property) {
-    if (pokemonList[property].height > 1.2) {
-    document.write('<p>' + 'Name: ' + pokemonList[property].name + ', Height: ' + pokemonList[property].height + 'm,' + ' (' + text + ')' + ' Type: ' + '<i>' + pokemonList[property].type[0] + ',  ' + pokemonList[property].type[1] + '</i>' + '.' +'</p>');
     } else {
-    document.write('<p>' + 'Name: ' + pokemonList[property].name + ', Height: ' + pokemonList[property].height + 'm,' + ' Type: ' + '<i>' + pokemonList[property].type[0] + ',  ' + pokemonList[property].type[1] + '</i>' + '.' +'</p>');  
+      console.log("Input needs: Name, Height and Type");
     }
-  })
-  
-   
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  function addListItem(pokemon) {
+      let pokeList2 = document.querySelector('.pokemon-list');
+      let pokeListItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('poke-button-class');
+      pokeListItem.appendChild(button);
+      pokeList2.appendChild(pokeListItem);
+      // add event listener to button:
+      button.addEventListener('click', function (event, secondEvent) {
+              showDetails(pokemon);
+       })
+  }
 
 
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+})();  
+
+
+//pokemonRepository.add('KAKINI');
+pokemonRepository.add({name: 'JavaScript', height: 100.7, type: ['monster', 'poison']});
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.addListItem(pokemon);
+});
+                
